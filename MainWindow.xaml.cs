c6cbde125f86ce554b91549c7d675080434d1e59
ozyours <roderick.tahir@hotmail.com>
2023-09-10 20:30:11 +0700

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace folder_cacher_net
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //StreamFile();
+            for (int i = 0; i < 8; i++)
+            {
+                var _folder_entry = new folder_entry();
+                lsb_Folder_List.Items.Add(_folder_entry);
+            }
+        }
+
+        public async void StreamFile()
+        {
+            var PATH = "D:\\Documents\\Gears\\SteamLibrary\\steamapps\\common\\Little Nightmares\\Atlas\\Content\\Paks\\Atlas-WindowsNoEditor.pak";
+
+            var _file1 = new FileStream(PATH, FileMode.Open);
+
+            var _info = new FileInfo(PATH);
+            var _stream_end = _info.Length;
+
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    _file1.ReadByte();
+                }
+            });
+
+            System.Environment.Exit(0);
+        }
+    }
+}
---
+++ folder_entry.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace folder_cacher_net
+{
+    /// <summary>
+    /// Interaction logic for folder_entry.xaml
+    /// </summary>
+    public partial class folder_entry : UserControl
+    {
+        public folder_entry()
+        {
+            InitializeComponent();
+        }
+
+
+        private void WriteStatus(string _text)
+        {
+            txt_Status.Text = _text;   
+        }
+
+        private async void Cache()
+        {
+            if (!Directory.Exists(txt_Path.Text))
+            {
+                WriteStatus("Error: Directory path is not exist.");
+                return;
+            }
+
+
+        }
+
+
+        private void btn_Remove_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_Resume_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_Cache_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
